8243ca509e89ae6239c085a7a467e11a1f26a008
akanshameshram <akanshameshram1994@gmail.com>
2019-09-03 23:04:25 +0530

    Added Cpp files for header files.

---
+++ Raylib/SnakeGame/Grid.cpp
@@ -0,0 +1,29 @@
+#include "Defines.h"
+#include "Grid.h"
+using namespace snakegame;
+using namespace math;
+
+Grid::Grid(int cellSize, int rows, int col)
+{
+
+}
+
+bool Grid::isInside(const math::INTPoint2D& point) const
+{
+
+}
+
+void Grid::generateFood()
+{
+
+}
+
+void Grid::getMinMax(math::INTPoint2D& min, math::INTPoint2D& max) const
+{
+
+}
+
+math::INTPoint2D& Grid::getFood() const
+{
+
+}
---
+++ Raylib/SnakeGame/Snake.cpp
@@ -0,0 +1,35 @@
+#include "Defines.h"
+#include "Snake.h"
+using namespace snakegame;
+using namespace math;
+
+Snake::Snake(int size)
+{
+
+}
+
+const math::INTPoint2D& Snake::head() const
+{
+
+}
+const math::INTPoint2D& Snake :: tail() const
+{
+
+}
+bool Snake::eat(const math::INTPoint2D& food)
+{
+
+}
+void Snake::move(Direction dir)
+{
+
+}
+bool Snake::isSelfCollision()
+{
+
+}
+const std::deque<math::INTPoint2D>& Snake::getBody() const
+{
+
+}
+
